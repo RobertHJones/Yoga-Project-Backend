1f5fe6965ce97816b351d46671a2ec2f5cc8b31b
RobertHJones <roberthuwjones@outlook.com>
2022-02-06 12:23:16 +0000

    created data

---
+++ poses.js
@@ -0,0 +1,12 @@
+// Yoga pose list
+
+export const poses = [
+  {
+    id: 1,
+    "Sanskrit name": "Hanumanasana",
+    "English name": "Splits",
+    instructions: "lorem ipsum",
+    series: "third",
+    image: "insert here",
+  },
+];
