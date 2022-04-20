fed21534d14bad26419020db2a16c17b1a27e1b0
RobertHJones <roberthuwjones@outlook.com>
2022-04-20 10:37:13 +0100

    cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 // import { databaseURL } from "./config.js";
 const app = express();
 
@@ -13,6 +14,7 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
 app.use(express.json());
+app.use(cors());
 
 import posesRouter from "./routes/poses.js";
 app.use("/poses", posesRouter);
